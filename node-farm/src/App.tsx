5e89dda5d61dc19bb0cb7b826e8ba0569b5e24ab
WolfY <david.skliarov.dev@gmail.com>
2026-05-05 14:33:15 +0400

    refactor: remove redundant styles

--- node-farm/src/App.tsx
+++ node-farm/src/App.tsx
@@ -4,12 +4,10 @@ import OverviewPage from "./pages/OverviewPage";
 export default function App() {
 	return (
 		// TODO: Add styles for the container
-		<div className="px-20 pt-20 pb-2 text-[62.5%]">
-			<Routes>
-				<Route path="/" element={<OverviewPage />} />
-				<Route path="/overview" element={<OverviewPage />} />
-				{/* <Route path="/product" Component={} /> */}
-			</Routes>
-		</div>
+		<Routes>
+			<Route path="/" element={<OverviewPage />} />
+			<Route path="/overview" element={<OverviewPage />} />
+			{/* <Route path="/product" Component={} /> */}
+		</Routes>
 	);
 }
