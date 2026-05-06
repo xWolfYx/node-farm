e5191028f73a1b97bab7c877c691049953567ba7
WolfY <david.skliarov.dev@gmail.com>
2026-05-06 16:08:02 +0400

    feat: add fetch logic for a single product

--- backend/index.js
+++ backend/index.js
@@ -6,17 +6,35 @@ import { fileURLToPath } from "node:url";
 const __fileName = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__fileName);
 
-const products = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const products = JSON.parse(
+	fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8"),
+);
 
 const server = http.createServer((req, res) => {
-	const pathName = req.url;
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	const fullURL = new URL(req.url, `http://${req.headers.host}`);
+	const pathName = fullURL.pathname;
 
-	if (pathName === "/" || pathName === "/overview")
-		res.end("This is the overview");
-	else if (pathName === "/product") res.end("This is the product");
+	// Product Page
+	if (pathName === "/" || pathName === "/overview") {
+		res.writeHead(200, { "content-type": "application/json" });
+		res.end(JSON.stringify(products));
+	} else if (pathName === "/product") {
+		const id = fullURL.searchParams.get("id");
+		const product = products.find((p) => p.id === Number(id));
+		res.writeHead(200, { "content-type": "application/json" });
+		res.end(JSON.stringify(product));
+	}
+	// API
 	else if (pathName === "/api") {
 		res.writeHead(200, { "content-type": "application/json" });
 		res.end(products);
+	} else {
+		res.writeHead(404, {
+			"content-type": "text/html",
+			message: "Page not found",
+		});
+		res.end("<h1>Page not found!</h1>");
 	}
 });
 
